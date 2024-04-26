d2e92fe90ff52773c31bc44234b63f4f9e930f70
coleyoung02 <coleyoung741@gmail.com>
2024-04-26 00:09:39 -0700

    new page layouts

--- buttonHandler.js
+++ buttonHandler.js
@@ -2,60 +2,28 @@ var mode = "light";
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 function onClick() {
     let sheets = document.getElementById('theme');
-    if(mode == "light") {
-        let photo = document.getElementById('photo-of-me');
-        if (photo) {
-            photo.src = "dark.jpg";
-        }
-        mode = "dark";    
+    if (mode == "light") {
+        mode = "dark";
         sessionStorage.setItem("clicked","dark");
     }
     else {
-        let photo = document.getElementById('photo-of-me');
-        if (photo) {
-            photo.src = "photo.jpg";
-        }
         mode = "light";
         sessionStorage.setItem("clicked","light");
     }
     sheets.href = mode + ".css";
 }
 function addHandler() {
-    let photo = document.getElementById('photo-of-me');
-    if (prefersDarkScheme.matches) {
-        if (photo) {
-            document.getElementById('photo-of-me').src = "dark.jpg";
-        }
-        mode = "dark";
-    }
     if (sessionStorage.getItem("clicked")) {
         console.log(sessionStorage.getItem("clicked"));
         mode = sessionStorage.getItem("clicked");
         var sheets = document.getElementById('theme');
         sheets.href = mode + ".css";
-        
-        if (photo) {
-            if(mode == "dark") {
-                photo.src = "dark.jpg";
-            }
-            else {
-                photo.src = "photo.jpg";
-            }
-        }
     }
     else {
-        mode = prefersDarkScheme.matches;
+        mode = prefersDarkScheme.matches ? 'dark' : 'light';
+        console.log(prefersDarkScheme.matches ? 'dark' : 'light')
         var sheets = document.getElementById('theme');
         sheets.href = mode + ".css";
-        
-        if (photo) {
-            if(mode == "dark") {
-                photo.src = "dark.jpg";
-            }
-            else {
-                photo.src = "photo.jpg";
-            }
-        }
     }
     document.getElementById("toggle_dark").addEventListener("click", onClick);
     

cb0bf2a5cd210909a87158457fef66224b0e1b82
coleyoung02 <94935033+coleyoung02@users.noreply.github.com>
2026-01-02 23:45:36 -0600

    gradual progress

--- buttonHandler.js
+++ buttonHandler.js
@@ -1,7 +1,6 @@
 var mode = "light";
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 function onClick() {
-    let sheets = document.getElementById('theme');
     if (mode == "light") {
         mode = "dark";
         sessionStorage.setItem("clicked","dark");
@@ -10,20 +9,41 @@ function onClick() {
         mode = "light";
         sessionStorage.setItem("clicked","light");
     }
-    sheets.href = sheets.dataset.pathrel + "/" + mode + ".css";
+    doSwap();
 }
 function addHandler() {
     if (sessionStorage.getItem("clicked")) {
         mode = sessionStorage.getItem("clicked");
-        var sheets = document.getElementById('theme');
-        sheets.href = sheets.dataset.pathrel + "/" + mode + ".css";
     }
     else {
         mode = prefersDarkScheme.matches ? 'dark' : 'light';
-        var sheets = document.getElementById('theme');
-        sheets.href = sheets.dataset.pathrel + "/" + mode + ".css";
     }
+    doSwap();
     document.getElementById("toggle_dark").addEventListener("click", onClick);
+}
+function doSwap() {
+    let sheets = document.getElementById('theme');
+    sheets.href = sheets.dataset.pathrel + "/" + mode + ".css";
+    let mapping;
+    if (mode==="light") {
+        mapping = {
+            "[bg-color]": "ffffff",
+            "[link-color]": "00660E"
+        }
+    }
+    else { 
+        mapping = {
+            "[bg-color]": "000000",
+            "[link-color]": "00CB22"
+        }
+    }
+    for (const element of document.getElementsByClassName("bandcamp")) {
+        let url = "https://bandcamp.com/EmbeddedPlayer/album=1173127828/size=large/bgcol=[bg-color]/linkcol=[link-color]/tracklist=false/artwork=small/track=2954856886/transparent=true/";
+        for (const [key, value] of Object.entries(mapping)) {
+            url = url.replace(key, value);
+        }
+        element.setAttribute("src", url)
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function (event) {
